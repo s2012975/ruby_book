0b01a320e014e4ae50a8c2a026e66e8465d1b817
s2012975 <bdips2012@gmail.com>
2021-04-10 18:15:29 +0900

    first commit

---
+++ lib/fizz_buzz.rb
@@ -0,0 +1,17 @@
+def fizz_buzz
+  for num in 1..100 do
+    if num % 15 == 0
+      puts "fizzbuzz"
+    elsif
+      num % 5 == 0
+      puts "buzz"
+    elsif
+      num % 3 == 0
+      puts "fizz"
+    else
+      puts num
+    end
+  end
+end
+
+puts 'Don\'t'
