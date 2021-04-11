400c8244753e6b4a9401eebdd4f663779556a76c
s2012975 <bdips2012@gmail.com>
2021-04-11 20:51:23 +0900

    rspec版test

---
+++ test/rspec.rb
@@ -0,0 +1,10 @@
+require 'RSpec'
+require_relative '../lib/fizz_buzz'
+
+RSpec.describe 'Fizz Buzz' do
+  example 'fizz_buzz' do
+    expect(fizz_buzz(1)).to eq 1
+    expect(fizz_buzz(2)).to eq 2
+    expect(fizz_buzz(3)).to eq 'Fizz'
+  end
+end
