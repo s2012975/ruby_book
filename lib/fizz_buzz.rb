166790a34293b390b58603dbff602827705f6828
s2012975 <bdips2012@gmail.com>
2021-04-11 20:27:25 +0900

    automatical_test

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,16 +1,28 @@
-def fizz_buzz
-  for num in 1..100 do
-    if num % 15 == 0
-      puts "fizzbuzz"
-    elsif
-      num % 5 == 0
-      puts "buzz"
-    elsif
-      num % 3 == 0
-      puts "fizz"
-    else
-      puts num
-    end
+def fizz_buzz(num)
+  if num % 15 == 0
+    p "fizzbuzz"
+  elsif
+    num % 5 == 0
+    p "buzz"
+  elsif
+    num % 3 == 0
+    p "Fizz"
+  else
+    p num
   end
 end
 
+for i in 1..100 do
+  fizz_buzz(i)
+end
+
+
+require 'minitest/autorun'
+
+class FizzBuzzTest < Minitest::Test
+  def test_fizz_buzz
+    assert_equal 1, fizz_buzz(1)
+    assert_equal 2, fizz_buzz(2)
+    assert_equal 'Fizz', fizz_buzz(3)
+  end
+end
