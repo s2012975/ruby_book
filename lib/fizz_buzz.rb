7b0ec8551f0db481fc0b11cd170b05bdcbea5cda
s2012975 <bdips2012@gmail.com>
2021-04-11 20:39:28 +0900

    テストコードを他のファイルに分離

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -15,14 +15,3 @@ end
 for i in 1..100 do
   fizz_buzz(i)
 end
-
-
-require 'minitest/autorun'
-
-class FizzBuzzTest < Minitest::Test
-  def test_fizz_buzz
-    assert_equal 1, fizz_buzz(1)
-    assert_equal 2, fizz_buzz(2)
-    assert_equal 'Fizz', fizz_buzz(3)
-  end
-end
