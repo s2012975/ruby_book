cb1c4db5c53550fb1ac5cde2de7c232017a218b8
s2012975 <bdips2012@gmail.com>
2021-04-23 17:09:21 +0900

    三項演算子

--- lib/array.rb
+++ lib/array.rb
@@ -1,18 +1,36 @@
-[].class
+# [].class
 
-a = [1, 2, 3]
+# a = [1, 2, 3]
 
-# a.delete_at(0)
-a.delete(3)
+# # a.delete_at(0)
+# a.delete(3)
 
-p a
+# print a
 
-# sum = 0
-# numbers.each do |n|
-#   sum += n
+# # sum = 0
+# # numbers.each do |n|
+# #   sum += n
+# # end
+# # sum
+
+# a = 1
+
+# puts a
+
+# numbers = [1,2,3,4]
+# sum = 0;
+# numbers.each do |i|
+#   sum += i
 # end
-# sum
 
-a = 1
+# puts sum
 
-puts a
+numbers = [1,2,3,4]
+sum = 0;
+numbers.each do |n|
+  # 三項演算子
+  sum_value = n.even? ? n * 10 :n
+  sum += sum_value
+end
+
+puts sum
