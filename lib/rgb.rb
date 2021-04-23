1bb816864739a6b3fb545af29f4828412dafd775
s2012975 <bdips2012@gmail.com>
2021-04-24 02:00:25 +0900

    [add]to_intsメソッド追加

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -5,3 +5,14 @@ def to_hex(r, g, b)
     hex += n.to_s(16).rjust(2, '0')
   end
 end
+
+def to_ints(hex)
+  r = hex[1..2]
+  b = hex[3..4]
+  g = hex[5..6]
+  ints = []
+  [r, b, g].each do |n|
+    ints << n.hex
+  end
+  ints
+end
