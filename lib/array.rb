f48925beae6a66e572f0c5700a91d1e6e3092e26
s2012975 <bdips2012@gmail.com>
2021-04-23 18:04:11 +0900

    inject/reduce/省力

--- lib/array.rb
+++ lib/array.rb
@@ -45,17 +45,37 @@
 # numbers.each {|n| new_numbers << n * 10}
 # p new_numbers
 
-# select
-numbers = [1, 2, 3]
-even_numbers = numbers.select{|n| n.even?}
-p even_numbers
+# # select
+# numbers = [1, 2, 3]
+# even_numbers = numbers.select{|n| n.even?}
+# p even_numbers
 
-# find_all
-numbers = [1, 2, 3]
-even_numbers = numbers.find_all{|n| n.even?}
-p even_numbers
+# # find_all
+# numbers = [1, 2, 3]
+# even_numbers = numbers.find_all{|n| n.even?}
+# p even_numbers
 
-# reject
+# # reject
+# numbers = [1, 2, 3]
+# even_numbers = numbers.reject{|n| n.even?}
+# p even_numbers
+
 numbers = [1, 2, 3]
-even_numbers = numbers.reject{|n| n.even?}
-p even_numbers
+non_multiples_of_three = numbers.reject {|n| n % 3 == 0 }
+p non_multiples_of_three
+
+# inject
+numbers = [1, 2, 3]
+sum = numbers.inject(0){ |result, n| result + n}
+p sum
+
+# reduce
+numbers = [1, 2, 3]
+sum = numbers.reduce(0){ |result, n| result + n}
+p sum
+
+p ['Mon', 'Tue', 'Wed'].inject('Sun'){ |result, n| result + n}
+
+&:で省力した描き方
+p ['a', 'b', 'c'].map {|s| s.upcase}
+p ['a', 'b', 'c'].map(&:upcase)
