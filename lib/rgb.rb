f5261215f16584b9ca8dd765956076ada3eab3b3
s2012975 <bdips2012@gmail.com>
2021-04-23 23:35:49 +0900

    リファクタリング2

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -1,8 +1,7 @@
 def to_hex(r, g, b)
   # '#' + r.to_s(16).rjust(2, '0') + g.to_s(16).rjust(2, '0') + b.to_s(16).rjust(2, '0')
   hex = '#'
-  [r, g, b].each do |n|
+  [r, g, b].inject('#') do |hex, n|
     hex += n.to_s(16).rjust(2, '0')
   end
-  hex
 end
