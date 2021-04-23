fa56bc68c203c6a78b58db951ec703135a8d5bd1
s2012975 <bdips2012@gmail.com>
2021-04-23 17:25:09 +0900

    map/collect

--- lib/array.rb
+++ lib/array.rb
@@ -25,12 +25,22 @@
 
 # puts sum
 
-numbers = [1,2,3,4]
-sum = 0;
-numbers.each do |n|
-  # 三項演算子
-  sum_value = n.even? ? n * 10 :n
-  sum += sum_value
-end
+# numbers = [1,2,3,4]
+# sum = 0;
+# numbers.each do |n|
+#   # 三項演算子
+#   sum_value = n.even? ? n * 10 :n
+#   sum += sum_value
+# end
 
-puts sum
+# puts sum
+
+# map/collect
+numbers = [1, 2, 3]
+new_numbers = numbers.map{|n| n * 10}
+p new_numbers
+
+numbers = [1, 2, 3]
+new_numbers = []
+numbers.each {|n| new_numbers << n * 10}
+p new_numbers
